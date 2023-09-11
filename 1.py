2beda6d4bfcc6b8c51d796a2b378f6cbe543a89a
KViktoria08 <1.viktoria.kurta@gmail.com>
2023-09-11 11:36:27 +0200

    2023.09.11
    oraimunka
    markdown lang

---
+++ 1.py
@@ -0,0 +1,20 @@
+nemjo=True
+
+while nemjo:
+    szam_str=(input("szam"))
+    if szam_str.isdecimal():
+        nemjo=False
+        szam=int(szam_str)
+
+
+
+
+
+if szam<3:
+    print("az x kisebb mint 3")
+
+elif szam>3:
+    print("az x nagyobb mint 3")  
+
+
+    
